e14cf4c27f1cef0ea8a7c6768dd43f2bca131746
camilaar99 <camiarguello1d@gmail.com>
2022-09-05 21:19:16 -0500

    conflicto

--- index4.js
+++ index4.js
@@ -1,12 +1,5 @@
 console.log("index4 hola mundo")
 
-<<<<<<< HEAD
-for (let i=0; i<=5; i++){
-    console.log(i)
-=======
-for ( let i =0; i <10; i++){
-    console.log('--Hola Mundo---' + i)
->>>>>>> 9a5543d237a8238f9b685b19ee1e1bfa75949c5f
 for ( let i =0; i <10; i++){
     console.log('--Hola Mundo---' + i)
 }
